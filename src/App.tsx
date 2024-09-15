c6cdef3a5c0483d537dbead7ea4e981c04498548
crazysrtguy <138736276+crazysrtguy@users.noreply.github.com>
2024-09-14 20:04:30 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -139,7 +139,7 @@ const App: React.FC = () => {
               <Hamster size={24} className="text-[#d4d4d4]" />
             </div>
             <div>
-              <p className="text-sm">CannaVerse Creator </p>
+              <p className="text-sm">Earn CannaVerse Tokens</p>
             </div>
           </div>
           <div className="flex items-center justify-between space-x-4 mt-1">
